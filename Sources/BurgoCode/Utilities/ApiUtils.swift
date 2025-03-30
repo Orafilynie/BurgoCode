0a723c99d6cdbe8d2bb5b74b093c2c94bcea91e6
Orafilynie <86892516+Orafilynie@users.noreply.github.com>
2025-03-30 18:56:09 +0200

    chore(api): added Connection header to avoid potential errors

--- Sources/BurgoCode/Utilities/ApiUtils.swift
+++ Sources/BurgoCode/Utilities/ApiUtils.swift
@@ -5,6 +5,7 @@ public class ApiUtils {
     public static func commonHeaders() -> [String: String] {
         return [
             "Accept": "application/json, text/plain, */*",
+            "Connection": "keep-alive",
             "Content-Type": "application/json",
             "Host": "webapi.burgerking.fr",
             "User-Agent": "com.unit9.bkFrApp/10.23.0",
